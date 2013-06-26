398a8758bc88b7f8f5c6f83491923b7f67bc2376
Tariq Daouda <tariq.daouda@umontreal.ca>
2013-06-26 18:45:29 -0400

    checkpoit

--- 2embryo.py
+++ 2embryo.py
@@ -14,12 +14,6 @@ def autoremove() :
 	
 	for c in toRemove :
 		conf.removeType(c)
-
-class __NotInstanciatedRaba :
-	__metaclass__ = type
-	def __init__(self, uniqueId, classType) :
-		self.uniqueId = uniqueId
-		self.classType = classType
 	
 class __Raba_MetaClass(type) :
 	def __new__(cls, name, bases, dct) :		
@@ -27,8 +21,8 @@ class __Raba_MetaClass(type) :
 		#exceptFields = set(('__module__', '__metaclass__', '__rabaObject'))
 		#primitiveTypes = ['str', 'int', 'float']
 		for k, v in dct.items():
-			#if k.find('__') != 0 and hasattr(v, '__rabaObject') and v.__rabaObject or type(v).__name__ in primitiveTypes :
-			if k[:2] != '__' :
+			#if k.find('__') != 0 and  or type(v).__name__ in primitiveTypes :
+			if k[0] != '_' :
 				fields.append(k)
 		
 		if name != 'Raba' :
@@ -38,26 +32,36 @@ class __Raba_MetaClass(type) :
 
 class Raba(object):
 	__metaclass__ = __Raba_MetaClass
-	__rabaObject = True
+	_rabaObject = True
 	
 	def __init__(self, uniqueId = None) :
-		self.load(uniqueId)
+		self._pupa = True
 
 	def load(self, uniqueId = None) :
-		self.fields = {}
-		self.values = {}
-		col = RABA_CONNECTION.cursor().execute('PRAGMA table_info(%s)' % self.__class__.__name__)
-		for c in col :
-			self.fields[c[0]] = c[1]
-			self.values[c[1]] = None
+		def getAttr(name) :
+			return object.__getattribute__(self, name)
+		
+		def setAttr(name, value) :
+			object.__setattr__(self, name, value)
 			
-		if uniqueId != None :
-			sql = ('SELECT * FROM %s WHERE id = ?' % self.__class__.__name__)
-			cur = RABA_CONNECTION.cursor()
-			row = cur.execute(sql, uniqueId).fetchone()
-			for i in range(len(row)) :
-				self.values[self.fields[i]] = row[i]
-	
+		if getAttr('_pupa') : :
+			fields = {}
+			columns = {}
+			col = RABA_CONNECTION.cursor().execute('PRAGMA table_info(%s)' % self.__class__.__name__)
+			for c in col :
+				fields[c[1]] = None
+				columns[c[0]] = c[1]
+				
+			if uniqueId != None :
+				sql = ('SELECT * FROM %s WHERE id = ?' % self.__class__.__name__)
+				cur = RABA_CONNECTION.cursor()
+				row = cur.execute(sql, uniqueId).fetchone()
+				for i in range(len(row)) :
+					self.fields[columns[i]] = row[i]
+					
+			setField('fields', fields)
+			setField('pupa', False)
+		
 	def __propagateRaba(self) :
 		for k, v in self.__dict__.items() :
 			if k[:2] != '__' :
@@ -73,12 +77,10 @@ class Raba(object):
 		self.fields[k] = v
 
 	def __getattribute__(self, name):
-		obj = object.__getattribute__(self, name)
-		if obj.__class__.__name__ == '__NotInstanciated' :
-			obj = obj.classType(obj.uniqueId)
-			object.__setattr__(self, name, obj)
-		return obj
-
+		if self.__pupa :
+			self.load()
+		return object.__getattribute__(self, name)
+		
 class Chromosome(Raba) :
 	#genes = RabaObjectList()
 	name = 'symb'
--- test.py
+++ test.py
@@ -1,19 +1,53 @@
-class RabaPrimitive(object) :
-	def __new__(cls, *args, **kwargs) :
-		class RabaPrimitive(args[0].__class__) :
-			__op__ = True
+import cPickle
+
+class Raba(object):
+	__rabaObject = True
+	
+	def __init__(self, uniqueId = None) :
+		self._pupa = True
+		self.uniqueId = uniqueId
 		
-		return RabaPrimitive(*args, **kwargs)
+	def load(self, uniqueId = None) :
+		def getField(name) :
+			return object.__getattribute__(self, name)
+		
+		def setField(name, value) :
+			object.__setattr__(self, name, value)
+			
+		if getField('_pupa') :
+			fields = {}
+			fields['a'] = 'aa'
+			setField('fields', fields)
+			setField('pupa', False)
 
+	def getPuppa(self) :
+		return self.__class__(self.uniqueId)
+		
+	def __getitem__(self, k) :
+		return self.fields[k]
 
-a = RabaPrimitive('sss')
-print a, hasattr(a, '__op__')
+	def __setitem(self, k, v) :
+		self.fields[k] = v
 
-class B :
+	def __getattribute__(self, name):
+		if object.__getattribute__(self, '_pupa') :
+			object.__getattribute__(self, 'load')()
+		return object.__getattribute__(self, name)
+
+class A :
 	def __init__(self) :
 		pass
-
-	def op(self) :
-		print 'op'
 	
-
+	def __propagateRaba(self) :
+		for k, v in self.__dict__.items() :
+			if k[0] != '_' :
+				if type(v).__name__  in ['str', 'int', 'float'] :
+					return v
+				elif hasattr(v, '_rabaObject') and v._rabaObject :
+					return v.getPuppa()
+				else :
+					return cPickle.dump(v)
+					
+r = Raba()
+print r.__dict__
+print r.__getattribute__, r.pupa
